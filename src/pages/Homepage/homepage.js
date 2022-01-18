ab63ff460d02b96f88dd61fd2aeee683244e3a8e
Ashutosh Sahoo <ashutoshsahoo27@gmail.com>
2022-01-18 20:59:49 +0530

    broken homepage into components

---
+++ src/pages/Homepage/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Directory from '../../components/Directory/Directory';
+import './homepage.scss';
+
+function Homepage() {
+    return (
+        <div className='homepage'>
+            <h1>Welcome to my Homepage</h1>
+            <Directory />
+        </div>
+    )
+}
+
+export default Homepage
