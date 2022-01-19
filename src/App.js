2cdd7a312e2a392d5fd7eba11bcec98beeafa9c8
Ashutosh Sahoo <ashutoshsahoo27@gmail.com>
2022-01-19 12:17:04 +0530

    added header component for app navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 import Homepage from './pages/Homepage/homepage';
 import Shoppage from './pages/ShopPage/Shoppage';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path={"/shop"} component={Shoppage} />
         <Route path={"/"} component={Homepage} />
